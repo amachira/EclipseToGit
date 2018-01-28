545dc47863ec7e5ae132386b2e9205eff125b39d
amachira <amachira@hawk.iit.edu>
2018-01-27 19:41:26 -0500

    String Reverse

--- src/JavaCode/StringReverse.java
+++ src/JavaCode/StringReverse.java
@@ -1,5 +1,17 @@
 package JavaCode;
 
 public class StringReverse {
+public static void Main(String args[])
+{
+	
 
+System.out.println("Enter the name");
+String name = null;
+String reverse = new StringBuffer(name).reverse().toString();
+reverse.toLowerCase();
+System.out.println(reverse);
+
+
+
+}
 }
